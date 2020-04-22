26e9ce04f18d7ced421ae3908695ad35e54dd9e8
Alisamar Husain <zrthxn@gmail.com>
2020-04-23 04:41:00 +0530

    Dev server

--- src/nodebuilder.js
+++ src/nodebuilder.js
@@ -1,4 +1,7 @@
 // this file will be run by package.json via scripts and will handle building node apps
 // this will have access to a list of node apps out of all the django apps
 
-console.log()
+/**
+ * @todo
+ * add dev server capability
+ */
